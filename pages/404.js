87fcbef75f292be0fc9cd9fb10d0d9db62618f2e
Alex <mr.shaljyan@gmail.com>
2022-08-20 22:47:54 +0400

    simple

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+const Error = () => (
+   
+    <h1>404 Error</h1>
+
+  );
+  
+  export default Error;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import MainContainer from "../components/MainContainer";
+
+
+const Index = () => {
+  return (
+
+    <MainContainer keywords={"main page"}>
+      <h1>Главная</h1>
+  </MainContainer>
+);
+  };
+
+export default Index;
